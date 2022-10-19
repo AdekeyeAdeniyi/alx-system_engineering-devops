652c231dac42c35991c5f75077565e39259ed35a
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-10-19 01:13:13 +0100

    print empolyee name and completed task using an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,46 +7,27 @@
     using a REST API and Empolyee ID.
 """
 
-import json
 import sys
-import urllib.request
+import requests
 
 employee_id = int(sys.argv[1])
 url = 'https://jsonplaceholder.typicode.com'
 
 
-def url_request(query, id=None):
-    """ Returns data from HTTP Request
-    """
-    if id:
-        with urllib.request.urlopen(url=f'{url}/{query}/{id}') as user_data:
-            body = user_data.read()
-            response = json.loads(body)
-            return response
-    else:
-        with urllib.request.urlopen(url=f'{url}/{query}/') as user_data:
-            body = user_data.read()
-            response = json.loads(body)
-            return response
-
-
 if __name__ == '__main__':
     if employee_id:
-        empolyee = url_request("users", employee_id)
-        todos = url_request("todos")
+        empolyee = requests.get(f'{url}/users/{employee_id}').json()
+        todos = requests.get(f'{url}/todos').json()
 
-        empolyee_name = empolyee.get('name')
         tasks = list(filter(lambda x: x.get('userId') == employee_id, todos))
         completed_task = list(filter(lambda x: x.get('completed'), tasks))
-        completed_task_no = len(completed_task)
-        total_task = len(tasks)
 
         print('Employee {} is done with tasks({}/{}):'.format(
-            empolyee_name,
-            completed_task_no,
-            total_task
+            empolyee.get('name'),
+            len(completed_task),
+            len(tasks)
         ))
 
-        if completed_task_no:
+        if  len(completed_task) > 0:
             for task in completed_task:
                 print('\t', task.get('title'))
