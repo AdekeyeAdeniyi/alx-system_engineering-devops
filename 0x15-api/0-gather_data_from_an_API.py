eb89d23433ccf19fd2bcc48574afc4386eda1b27
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-10-19 01:17:21 +0100

    print empolyee name and completed task using an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,28 +6,29 @@
         - Lists of completed task
     using a REST API and Empolyee ID.
 """
-
+import re
 import sys
 import requests
 
-employee_id = int(sys.argv[1])
 url = 'https://jsonplaceholder.typicode.com'
 
 
 if __name__ == '__main__':
-    if employee_id:
-        empolyee = requests.get(f'{url}/users/{employee_id}').json()
-        todos = requests.get(f'{url}/todos').json()
+    if len(sys.argv) > 0:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            employee_id = int(sys.argv[1])
+            empolyee = requests.get(f'{url}/users/{employee_id}').json()
+            todos = requests.get(f'{url}/todos').json()
 
-        tasks = list(filter(lambda x: x.get('userId') == employee_id, todos))
-        completed_task = list(filter(lambda x: x.get('completed'), tasks))
+            tasks = list(filter(lambda x: x.get('userId') == employee_id, todos))
+            completed_task = list(filter(lambda x: x.get('completed'), tasks))
 
-        print('Employee {} is done with tasks({}/{}):'.format(
-            empolyee.get('name'),
-            len(completed_task),
-            len(tasks)
-        ))
+            print('Employee {} is done with tasks({}/{}):'.format(
+                empolyee.get('name'),
+                len(completed_task),
+                len(tasks)
+            ))
 
-        if  len(completed_task) > 0:
-            for task in completed_task:
-                print('\t', task.get('title'))
+            if  len(completed_task) > 0:
+                for task in completed_task:
+                    print('\t', task.get('title'))
