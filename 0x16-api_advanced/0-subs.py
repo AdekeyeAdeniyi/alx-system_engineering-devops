87aa742c7355b048012e4b6772c902b71298679f
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-11-03 15:21:14 +0100

    return number of subscribers for a given Reddit account

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,7 @@
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
         Return total number of subscribers of a `Reddit User`
@@ -13,18 +14,16 @@ def number_of_subscribers(subreddit):
             subreddit(str): reddit account user name
 
         Return(int):
+        ------------
             Number of Subscribers
     """
-
     response = requests.get(
-        'https://www.reddit.com/r/{}/about.json?limit=10'.format(subreddit),
+        'https://www.reddit.com/r/{}/about'.format(subreddit),
         headers={'User-Agent': 'Mozilla/5.0'},
         allow_redirects=False
     )
 
-    body = response.json()
-
-    if (response.status_code != 404):
-        return (body.get('data').get('subscribers'))
+    if response.status_code != 404:
+        return (response.json().get('data').get('subscribers'))
     else:
         return (0)
