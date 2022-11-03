56a46a7e0214c17b90d2f60a269d97dc1552268c
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2022-11-03 16:01:20 +0100

    return first `10` posta of a Reddit account

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    Returns first `10` posts of a Reddit account
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+        Print first `10` posts of a `Reddit User`
+
+        Parameters
+        ----------
+            subreddit(str): reddit account user name
+    """
+    response = requests.get(
+        'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit),
+        headers={'User-Agent': 'Mozilla/5.0'},
+        allow_redirects=False
+    )
+    if response.status_code == 200:
+        posts = response.json().get('data').get('children')
+        for post in posts:
+            print(post.get('data').get('title'))
+    else:
+        print(None)
